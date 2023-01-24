680387354a58b7e3d19f69b5195f61e21a3e7120
Bharat saini <sainib601@gmail.com>
2023-01-24 06:56:51 +0530

    PORT solved

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const app = require("./app");
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT,()=>{
     console.log(`Server started at :- ${PORT}`)
 })
 
+
 process.on('unhandledRejection',(err)=>{
     console.log(err)
 })
+
